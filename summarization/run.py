f8730ee5c6b439f98362c48726525b2f881a7a85
majorcrimesunit <amanrai@Amans-MacBook-Pro.local>
2019-04-29 11:01:06 +0530

    minor_changes

--- summarization/run.py
+++ summarization/run.py
@@ -10,5 +10,9 @@ max_doc_length = 100
 max_summary_length = 10
 _cuda = torch.cuda.is_available()
     
-d, se, m, su, po = genBatch(data=network_testing_data, _cuda = _cuda, max_doc_length = 100, max_summary_length=12)
+d, se, m, su, po = genBatch(data=network_testing_data, 
+                            _cuda = _cuda, 
+                            max_doc_length = max_doc_length, 
+                            max_summary_length=max_summary_length)
+
 print(d.size(), se.size(), m.size(), su.size(), po.size())
