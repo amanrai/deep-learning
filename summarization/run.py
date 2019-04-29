fcf035bee66185a037c9da70907ce8e3a8893dfe
majorcrimesunit <amanrai@Amans-MacBook-Pro.local>
2019-04-29 12:55:37 +0530

    ...

--- summarization/run.py
+++ summarization/run.py
@@ -8,7 +8,8 @@ from pytorch_pretrained_bert import BertTokenizer
 from pytorch_pretrained_bert import BertModel
 
 network_testing_data = pickle.load(open("./network_testing.pickle", "rb"))
-print(len(network_testing_data))
+
+wordCriterion = torch.nn.CrossEntropyLoss()
 
 max_doc_length = 100
 max_summary_length = 10
@@ -43,17 +44,18 @@ _prev_word = _prev_word.repeat(bs, 1)
 gen_words = []
 gen_atts = []
 
-
 _d, _ = bert(d, se, m, output_all_encoded_layers = False)
 _d = _d * m.unsqueeze(-1).float()   
 
 for i in range(5):
+    act_words = su[:,i]
+    print(act_words.size())
     new_words, atts, _hs = s.forward(_d, _hs, _prev_word)
     actual_words = F.softmax(new_words, dim=-1)
     actual_words = torch.max(actual_words, dim=-1)[1]
     _prev_word = actual_words.unsqueeze(-1)
     gen_words.append(_prev_word.detach())
     gen_atts.append(atts.detach())
-
+    loss = wordCriterion(new_words, act_words)
+    print(loss.data.item())
 print(gen_words)
-print(gen_atts)
