5e5cdeb71df314595d6e36be9968589ae2780410
Aman Rai <aman.rai25@gmail.com>
2019-05-01 08:17:56 +0530

    ...

--- summarization/eval.py
+++ summarization/eval.py
@@ -11,5 +11,24 @@ from pytorch_pretrained_bert import BertModel
 import random
 import argparse
 
+
+parser = argparse.ArgumentParser(description='')
+parser.add_argument('--eval_model', 
+                    type=str, 
+                    help='model to continue training', 
+                    default="summarizer_BestTrainingLoss.h5")
+
+args = parser.parse_args()
+
 testing = pickle.load(open("./network_testing.pickle", "rb"))
 print(len(testing))
+
+_cuda = torch.cuda.is_available()
+if (_cuda):
+    print("Cuda is available:", torch.cuda.device_count(), "GPUs.")
+    _bs = args.bs * torch.cuda.device_count()
+
+print("Loading Model...")    
+
+network = BertSummarizer(isCuda = _cuda)
+network.load_state_dict(torch.load(args.eval_model))
